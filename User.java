5476d15b4a40a9ad860125a546cf6997d9431a19
mdathathreya16 <mdathathreya16@gmail.com>
2026-02-07 09:31:03 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+public class User
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("ente a value="+a);
+        int b = sc.nextInt();
+        System.out.println("enter b value="+b);
+        int sum = a+b;
+        System.out.println("The sum="+sum);
+        float c = sc.nextFloat();
+        System.out.println("Enter c value:="+c);
+        double d = sc.nextDouble();
+        System.out.println("Enter d value:="+d);
+        String n = sc.nextLine();
+        System.out.println("Enter n value:="+n);
+
+
+    }
+}
+    
+
+
