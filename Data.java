2d97f21275ed9c0de862ff64bb9f0fbc1e88c4dc
mdathathreya16 <mdathathreya16@gmail.com>
2026-02-07 10:57:10 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,23 @@
+public class Data
+{
+    public static void main(String[] args)
+    {
+        String name = "Ram";
+        int age = 23;
+        int marks = 98;
+        System.out.println("name="+name);
+        System.out.println("age="+age);
+        System.out.println("marks="+marks);
+    }
+
+
+       
+    
+
+
+    
+        
+    
+}
+        
+
